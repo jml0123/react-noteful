b22dae1f0db139352bb69bee5dd65663609051a8
Miguel Lorenzo <jsmglorenzo@gmail.com>
2020-06-15 00:53:02 -0400

    deploy to heroku

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-    // Local Server for now
-    API_ENDPOINT: "http://localhost:8000/api/",
+    API_ENDPOINT: "https://fathomless-basin-48045.herokuapp.com/api/",
     API_KEY: null 
 }
